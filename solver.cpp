e86fbb834e5d00f3229c83dd00c181db706e806f
Borroot <borroot@live.nl>
2019-10-22 22:19:59 +0200

    Change shutdown value.

--- solver.cpp
+++ solver.cpp
@@ -86,7 +86,7 @@ bool DFS_sequential_search (vector<V> vertices, vector<V> &chosen, vector<vector
 
 	for (int i = 0; i < vertices.size(); i++) {
 		total++;
-		if (total > 7000) {
+		if (total > 8000) {
 			return false;
 		}
 		V current = vertices[i];
