cc94cdb123272611abde3c2c1f452d59179212a4
Borroot <borroot@live.nl>
2019-10-30 23:17:32 +0100

    Solver with colors works.

--- solver.cpp
+++ solver.cpp
@@ -1,47 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <functional>
-#include <algorithm>
 #include <bits/stdc++.h> 
+
+#define COLORS 5
 
 using namespace std;
 
-int pruned = 0, total = 0;
-
 typedef int V;
-
-struct State {
-	V current;
-	vector<V> vertices;
-	vector<V> chosen;
-};
-
-bool operator== (const State &state1, const State &state2) {
-	return state1.current == state2.current && state1.vertices == state2.vertices && state1.chosen == state2.chosen;	
-}
-
-namespace std {
-	template<> struct hash<vector<V>> {
-		size_t operator() (const vector<V> &vertices) const noexcept {
-			size_t seed = vertices.size();
-			for(int i = 0; i < vertices.size(); i++) {
-				seed += vertices[i] * 37;
-			}
-			return hash<size_t>{}(seed);
-		}
-	};
-}
-
-namespace std {
-	template<> struct hash<State> {
-		size_t operator() (const State &state) const noexcept {
-			size_t h1 = hash<V>()(state.current);
-			size_t h2 = hash<vector<V>>()(state.vertices);
-			size_t h3 = hash<vector<V>>()(state.chosen);
-			return h1 ^ h2 ^ h3;
-		}
-	};
-}
 
 void init_vectors (vector<V> &vertices, vector<vector<V>> &neighbors, const int &V_SIZE, const int &E_SIZE) {
 
@@ -60,53 +25,68 @@ void init_vectors (vector<V> &vertices, vector<vector<V>> &neighbors, const int 
 	}
 }
 
-void insert(vector<V> &vertices, V vertex) {
-	vector<V>::iterator it = lower_bound(vertices.begin(), vertices.end(), vertex, greater<V>()); 
-	vertices.insert(it, vertex); 
-}
-
-void remove_vertex (vector<V> &vertices, V vertex) {
+void remove_vertex (vector<V> &vertices, const V vertex) {
 	vertices.erase(remove(vertices.begin(), vertices.end(), vertex), vertices.end());
 }
 
-vector<V> create_next_vertices (vector<V> &vertices, vector<vector<V>> &neighbors, V current) {
-	vector<V> next_vertices(vertices);	
-	remove_vertex(next_vertices, current);
-
-	for (int i = 0; i < neighbors[current].size(); i++) {
-		remove_vertex(next_vertices, neighbors[current][i]);
+int highest (const vector<V> &colors) {
+	vector<int> counts(COLORS);
+	for (int i = 0; i < colors.size(); i++) {
+		counts[colors[i]]++;
 	}
-	return next_vertices;
+
+	int max = -1;
+	for (int i = 0; i < counts.size(); i++) {
+		if (counts[i] > max) {
+			max = counts[i];
+		}
+	}
+	return max;
 }
 
-bool DFS_sequential_search (vector<V> vertices, vector<V> &chosen, vector<vector<V>> &neighbors, unordered_set<State> &states, const int &BIN_GOAL) {
-	if (chosen.size() >= BIN_GOAL) {
-		return true;
+int choose_color (const V &current, const vector<V> &colors, const vector<vector<V>> &neighbors) {
+	bool legal_color[COLORS];
+	for (int i = 0; i < COLORS; i++) {
+		legal_color[i] = true;
 	}
 
-	for (int i = 0; i < vertices.size(); i++) {
-		total++;
-		V current = vertices[i];
-		insert(chosen, current);
-		vector<V> next_vertices = create_next_vertices(vertices, neighbors, current);
+	for (int i = 0; i < neighbors[current].size(); i++) {
+		legal_color[colors[neighbors[current][i]]] = false;
+	}
 
-		State state = {current, next_vertices, chosen};
-		if (states.find(state) == states.end()) {
-			states.insert(state);
-
-			if (DFS_sequential_search(next_vertices, chosen, neighbors, states, BIN_GOAL)) {
-				return true;
-			}
-		} else {
-			pruned++;
+	for (int i = 0; i < COLORS; i++) {
+		if (legal_color[i]) {
+			return i;
 		}
+	}
+	return COLORS - 1;
+}
 
-		remove_vertex(chosen, current);
-	}		
-	return false;
+int color (vector<V> vertices, const vector<vector<V>> &neighbors, const int &V_SIZE) {
+	vector<V> colors(V_SIZE);
+
+	while (vertices.size() > 0) {
+		V current = vertices[rand() % vertices.size()];	
+		colors[current] = choose_color(current, colors, neighbors);
+		remove_vertex(vertices, current);
+	}
+	return highest(colors);
+}
+
+bool search (const vector<V> &vertices, const vector<vector<V>> &neighbors, const int &V_SIZE, const int &BIN_GOAL) {
+	const int MAX = 10000;
+	int result, count = 0;
+
+	do {
+		result = color (vertices, neighbors, V_SIZE);
+		count++;
+	} while (result < BIN_GOAL && count < MAX);
+
+	return result >= BIN_GOAL;
 }
 
 int main () {
+	srand(time(0));
 
 	int E_SIZE, V_SIZE, BIN_GOAL;
 	cin >> E_SIZE;
@@ -116,14 +96,10 @@ int main () {
 	vector<V> vertices;
    	vector<vector<V>> neighbors(V_SIZE);
 
-	vector<V> chosen;
-	unordered_set<State> states;
-
 	init_vectors(vertices, neighbors, V_SIZE, E_SIZE);
+	bool result = search(vertices, neighbors, V_SIZE, BIN_GOAL);
 
-	bool result = DFS_sequential_search(vertices, chosen, neighbors, states, BIN_GOAL);
 	cout << (result ? "possible" : "impossible") << endl;
-	cerr << "pruned: " << pruned << "\ttotal: " << total << "\trelative: " << (int)((pruned * 100.0f) / total) << "%" << endl; 
 
 	return 0;
 }
