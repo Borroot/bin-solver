ad06d49652f6f169e53157076c9b717e2bebbb09
Borroot <borroot@live.nl>
2019-11-03 16:38:08 +0100

    Add comments and convert vector to int[].

--- solver-coloring.cpp
+++ solver-coloring.cpp
@@ -26,17 +26,26 @@ void init_vectors (vector<V> &vertices, vector<vector<V>> &neighbors, const int 
 }
 
 void remove_vertex (vector<V> &vertices, const V vertex) {
+	// Remove vertex from the vertices list by its id not by index.
 	vertices.erase(remove(vertices.begin(), vertices.end(), vertex), vertices.end());
 }
 
 int highest (const vector<V> &colors) {
-	vector<int> counts(COLORS);
+	int counts[COLORS];
+
+	// Initialize all counts to 0.
+	for (int i = 0; i < COLORS; i++) {
+		counts[i] = 0;
+	}
+
+	// Increase all the corresponding color counts for all vertex colors.
 	for (int i = 0; i < (int)colors.size(); i++) {
 		counts[colors[i]]++;
 	}
 
+	// Get the biggest color count.
 	int max = -1;
-	for (int i = 0; i < (int)counts.size(); i++) {
+	for (int i = 0; i < COLORS; i++) {
 		if (counts[i] > max) {
 			max = counts[i];
 		}
@@ -46,25 +55,31 @@ int highest (const vector<V> &colors) {
 
 int choose_color (const V &current, const vector<V> &colors, const vector<vector<V>> &neighbors) {
 	bool legal_color[COLORS];
+
+	// Initialize all the colors to legal.
 	for (int i = 0; i < COLORS; i++) {
 		legal_color[i] = true;
 	}
 
+	// Set the color to illegal if one of the neighbors has this color.
 	for (int i = 0; i < (int)neighbors[current].size(); i++) {
-		legal_color[colors[neighbors[current][i]]] = false;
+		int color_neighbor = colors[neighbors[current][i]];
+		legal_color[color_neighbor] = false;
 	}
 
+	// Take the FIRST legal color (so lower index colors have priority).
 	for (int i = 0; i < COLORS; i++) {
 		if (legal_color[i]) {
 			return i;
 		}
 	}
-	return COLORS - 1;
+	return -1;
 }
 
 int color (vector<V> vertices, const vector<vector<V>> &neighbors, const int &V_SIZE) {
 	vector<V> colors(V_SIZE);
 
+	// Choose a color for a random vertex until all vertices are colored.
 	while (vertices.size() > 0) {
 		V current = vertices[rand() % vertices.size()];	
 		colors[current] = choose_color(current, colors, neighbors);
@@ -77,6 +92,8 @@ bool search (const vector<V> &vertices, const vector<vector<V>> &neighbors, cons
 	const int MAX = 10000;
 	int result, count = 0;
 
+	// Search for a coloring which satisfies the BIN_GOAL until the limit is reached.
+	// If the limit is reached stop and return false else return true.
 	do {
 		result = color (vertices, neighbors, V_SIZE);
 		count++;
