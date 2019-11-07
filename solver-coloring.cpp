69f07a6bc0b5c377d413616bc680b9ea9ae2e756
Borroot <borroot@live.nl>
2019-11-07 20:26:03 +0100

    Solved all.

--- solver-coloring.cpp
+++ solver-coloring.cpp
@@ -8,6 +8,20 @@ using namespace std;
 
 typedef int V;
 
+vector<V> results;
+
+ostream &operator << (ostream &out, vector<V> vertices) {
+	cerr << "[";
+	for (int i = 0; i < (int)vertices.size(); i++) {
+		cerr << vertices[i]+1;
+		if (i != (int)vertices.size()-1) {
+			cerr << ",";
+		}
+	}
+	cerr << "]";
+	return out;
+}
+
 void init_vectors (vector<V> &vertices, vector<vector<V>> &neighbors, const int &V_SIZE, const int &E_SIZE) {
 
 	// Initialize vertices list.
@@ -42,13 +56,23 @@ int highest (const vector<V> &colors) {
 		counts[colors[i]]++;
 	}
 
+	int color_max = -1;
 	// Get the biggest color count.
 	int max = -1;
 	for (int i = 0; i < COLORS; i++) {
 		if (counts[i] > max) {
 			max = counts[i];
+			color_max = i;
 		}
 	}
+
+	results.clear();
+	for (int i = 0; i < (int)colors.size(); i++) {
+		if (colors[i] == color_max) {
+			results.push_back(i+1);
+		}
+	}
+
 	return max;
 }
 
@@ -77,6 +101,9 @@ int choose_color (const V &current, const vector<V> &colors, const vector<vector
 
 int color (vector<V> vertices, const vector<vector<V>> &neighbors, const int &V_SIZE) {
 	vector<V> colors(V_SIZE);
+	for (int i = 0; i < V_SIZE; i++) {
+		colors[i] = -1;
+	}
 
 	// Choose a color for a random vertex until all vertices are colored.
 	while (vertices.size() > 0) {
@@ -101,6 +128,62 @@ bool search (const vector<V> &vertices, const vector<vector<V>> &neighbors, cons
 	return result >= BIN_GOAL;
 }
 
+void remove_edges (const V &vertex, vector<vector<V>> &neighbors) {
+	// Remove all the references to vertex inside of the neighbors from other vertices.
+	for (int i = 0; i < (int)neighbors.size(); i++) {
+		remove_vertex(neighbors[i], vertex);
+	}
+}
+
+bool subset (const vector<V> &vertices1, const vector<V> &vertices2) {
+	// Determine whether vertices1 is a subset of vertices2.
+	for (int i = 0; i < (int)vertices1.size(); i++) {
+		bool found = false;;
+
+		for (int j = 0; !found && j < (int)vertices2.size(); j++) {
+			if (vertices1[i] == vertices2[j]) {
+				found = true;
+			}
+		}
+		if (!found) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void filter (vector<V> &vertices, vector<vector<V>> &neighbors) {
+	// Here we filter out vertices based on the following criteria:
+	// if the neighbors of the current vertex are a subset of the 
+	// neighbors of a neighbor of the current vertex then remove this
+	// neighbor from the vertices left. This can be done since we 
+	// know that it is always more beneficial to choose the current
+	// vertex instead of this particular neighbor. 
+	bool found;
+
+	do {
+		found = false;
+
+		for (int i = 0; i < (int)vertices.size() && !found; i++) {
+			V current = vertices[i];
+
+			for (int j = 0; j < (int)neighbors[current].size(); j++) {
+				V neighbor = neighbors[current][j];
+				remove_vertex(neighbors[current], neighbor);
+
+				if (subset(neighbors[current], neighbors[neighbor])) {
+					remove_edges(neighbor, neighbors);	
+					remove_vertex(vertices, neighbor);
+
+					found = true;
+				} else {
+					neighbors[current].push_back(neighbor);
+				}
+			}
+		}
+	} while (found);
+}
+
 int main () {
 	srand(time(0));
 
@@ -110,10 +193,24 @@ int main () {
 	vector<V> vertices;
    	vector<vector<V>> neighbors(V_SIZE);
 
+	vector<V> chosen;
+
 	init_vectors(vertices, neighbors, V_SIZE, E_SIZE);
+
+	// Filter out all vertices which will not be in the solution anyways.
+	filter(vertices, neighbors);
+
+	// If there are less vertices left than our bin goal, well then it is impossible to place them.
+	if ((int)vertices.size() < BIN_GOAL) {
+		cout << "impossible" << endl;
+		return 0;
+	}
+
+	// Search for a solution.
 	bool result = search(vertices, neighbors, V_SIZE, BIN_GOAL);
 
 	cout << (result ? "possible" : "impossible") << endl;
+	cerr << results << endl;
 
 	return 0;
 }
